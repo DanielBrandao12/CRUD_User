327eb82f302442a131e7b40f2ba45f4d7cf5893d
DanielBrandao12 <drb12da@gmail.com>
2023-01-21 17:58:13 -0300

    criação get, post, put e delete user

---
+++ controllers/userController.js
@@ -0,0 +1,62 @@
+const database = require("../database/models");
+
+
+function getUsers(req, res){
+  
+  database.User.findAll().then((data) =>{
+    res.json(data)
+  })
+}
+
+function getUserById(req, res) {
+  const { id } = req.params;
+  database.User.findByPk(id).then((data) => {
+    return res.json(data);
+  });
+}
+
+function createUser(req, res) {// Criar ou alterar - Validações são obrigatórias
+    const { nome, email, senha } = req.body;
+    database.User.create({
+      Nome: nome,
+      Email: email,
+      Senha: senha,
+   
+    }).then((data) => {
+      return res.json(data);
+    });
+  }
+
+  function updateUser(req, res){
+    let {id} = req.params
+    let  { nome, email, senha} = req.body
+    database.User.update({
+      Nome: nome,
+      Email: email,
+      Senha: senha,
+    },
+    {
+      where: {
+        Id: id
+      }
+    })
+
+    res.redirect('/user')
+  }
+
+  function deleteUser(req, res) {
+
+    let { id } = req.params;
+  
+    database.User.destroy({ where: { id, } });
+  
+    res.redirect('/user');
+  }
+
+  module.exports = {
+    createUser,
+    getUserById,
+    getUsers,
+    deleteUser,
+    updateUser
+  }
---
+++ database/models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+      "User",
+      {
+        Id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        Nome: {
+          type: DataTypes.STRING,
+        },
+        Email: {
+          type: DataTypes.STRING,
+        },
+        Senha: {
+          type: DataTypes.STRING,
+        }
+     
+      },
+      {
+        tableName: "users",
+        timestamps: false,
+      }
+    );
+
+    return User;
+    }
